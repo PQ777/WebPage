3fee69800e74adb7abecf1708505bf7e8f519f73
InHo Choi (PQ77) <dlsgh9230@naver.com>
2023-11-08 20:25:11 +0900

    로그인창 수정

---
+++ public/login.js
@@ -0,0 +1,15 @@
+function checkLogin()
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username == "username" && password == "password")
+    {
+        document.getElementById("message").innerText = "로그인 성공";
+        window.location.href = "login.html";
+    }
+    else
+    {
+        document.getElementById("message").innerText = "로그인 실패";
+    }
+}
